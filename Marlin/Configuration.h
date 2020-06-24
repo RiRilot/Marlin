48e8eaface36c747afc3b48e6b82712e1bddc63b
RiRilot <ri.rilot@gmail.com>
2020-06-24 19:49:54 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -956,7 +956,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 35, -25, -2.48 }
+#define NOZZLE_TO_PROBE_OFFSET { -45.16, 0, -2.48 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1096,8 +1096,8 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -33
-#define Y_MIN_POS -6
+#define X_MIN_POS -15
+#define Y_MIN_POS -4
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
